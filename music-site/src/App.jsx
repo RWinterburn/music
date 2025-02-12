80d32fb19757e1ec917503339a883220949871d6
Rob Winterburn <forestalwinters@gmail.com>
2025-02-12 15:27:23 +0000

    adding images and scrolling

---
+++ music-site/src/App.jsx
@@ -0,0 +1,27 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import TextBlock from './textBlock';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div class="animation_layer parallax" id="Abbey"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="River"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.1}>
+          <div class="animation_layer parallax" id="Farm"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="City"></div>
+        </ParallaxLayer>
+
+      </Parallax>
+    </div>
+  );
+}
+
+export default App;
---
+++ music-site/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  
+    server: {
+      allowedHosts: ['5173-rwinterburn-music-bpeht9zozk2.ws-eu117.gitpod.io'],
+    },
+})
